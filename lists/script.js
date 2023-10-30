939b6f997b5dc7df87b859bd66770024fb0962b5
Łukasz Tshipenchko <dev@zxc.sx>
2023-10-31 01:20:39 +0600

    Apply dark theme

--- lists/script.js
+++ lists/script.js
@@ -10,7 +10,6 @@ $(() => {
     // It is the same as $(document).ready(function() { ... });
     // This block will be executed once the page has been loaded
     // Some kind of unnecessary code, when the script tag is at the end of the body
-    $(".flex-grow-1").css("background-color", "red");
 
     if (!todoService.lists.length) {
         for (let i = 0; i < 5; i++) {
