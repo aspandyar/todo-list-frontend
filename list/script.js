5984823030f7eee0e4d81e8c6aebcf43989e1201
Łukasz Tshipenchko <dev@zxc.sx>
2023-10-31 14:52:34 +0600

    Luka (#11)
    * Add `deleteTodoFromList` method
    
    * Better UI for the list

--- list/script.js
+++ list/script.js
@@ -53,22 +53,63 @@ $(() => {
         event.stopPropagation();
     });
 
+    const $title = $form.find("#title");
+    const $description = $form.find("#description");
+    const $deadline = $form.find("#deadline");
+    const $priority = $form.find("#priority");
+
+    $modal.on("hidden.bs.modal", () => {
+        $form.trigger("reset");
+        $title.removeClass("is-invalid");
+        $description.removeClass("is-invalid");
+        $deadline.removeClass("is-invalid");
+        $priority.removeClass("is-invalid");
+    });
+    $title.on("input", () => $title.removeClass("is-invalid"));
+    $description.on("input", () => $description.removeClass("is-invalid"));
+    $deadline.on("input", () => $deadline.removeClass("is-invalid"));
+
     $confirm.click(() => {
-        const title = $form.find("#title").val();
-        const description = $form.find("#description").val();
-        const date = $form.find("#date").val();
-        const priority = $form.find("#priority").val();
-        todoService.addTodoToList(listId, title, description, date, priority);
+        const title = $title.val();
+        const description = $description.val();
+        const deadline = $deadline.val();
+        const priority = $priority.val();
+
+        let isValid = true;
+        if (!title) {
+            $title.addClass("is-invalid");
+            isValid = false;
+        }
+        if (!description) {
+            $description.addClass("is-invalid");
+            isValid = false;
+        }
+        if (!deadline) {
+            $deadline.addClass("is-invalid");
+            isValid = false;
+        }
+
+        if (!isValid) {
+            // TODO: play sound
+            // TODO: show alert
+            return;
+        }
+
+        todoService.addTodoToList(listId, title, description, deadline, priority);
         eventTodosChanged();
 
         // noinspection JSUnresolvedReference
         $modal.modal("toggle");
 
-        task_created.play()
+        task_created.play().then();
     });
 
     $confirm.mouseover(() => {
         $confirm.attr("title", "Click to confirm");
+    })
+
+    $close.mouseover(() => {
+        $close.attr("title", "Click to close");
     })
 })
 
@@ -76,7 +117,7 @@ const renderTodos = () => {
     const $todos = $("#todos");
     $todos.empty();
     todoService.getListById(listId).todos.forEach(todo => {
-        $todos.append($(`
+        const $todo = $(`
             <div class="todo card bg-secondary p-3 mb-3 rounded-3">
                 <h4>${todo.title}</h4>
                 <p>${todo.description}</p>
@@ -86,7 +127,15 @@ const renderTodos = () => {
                     <button class="btn btn-danger btn-lg ms-1">Delete</button>
                 </div>
             </div>
-        `));
+        `);
+
+        $todo.find(".btn-danger").click(() => {
+            // TODO: play sound
+            todoService.deleteTodoFromList(listId, todo.id);
+            eventTodosChanged();
+        });
+
+        $todos.append($todo);
     });
 }
 
