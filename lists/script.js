79fc4e98a10e623a2425108cde050aab78a1566b
aspandyar <aspandyart@gmail.com>
2023-10-31 15:31:02 +0600

    com

--- lists/script.js
+++ lists/script.js
@@ -132,19 +132,19 @@ const renderLists = () => {
 }
 
 // using snippet
-let mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0;
-} 
+// function topFunction() {
+//   document.body.scrollTop = 0; 
+//   document.documentElement.scrollTop = 0;
+// } 
